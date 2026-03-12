0930fb6d89ec55d0cb9f0f5eb9bb026ad57ae3b5
Joao Victor <jvpereirarocha@gmail.com>
2026-03-12 07:48:38 -0300

    feat: add meta attributes to routes and create logic to check if the user is already authenticated before redirect him

--- src/router/index.js
+++ src/router/index.js
@@ -6,26 +6,26 @@ import RegisterView from '@/features/auth/views/RegisterView.vue'
 import HomeView from '@/features/dashboard/views/HomeView.vue'
 import TransactionsView from '@/features/transactions/views/TransactionsView.vue'
 import TransactionUpsertView from '@/features/transactions/views/TransactionUpsertView.vue'
+import { useAuthStore } from '@/features/auth/store/authStore'
 
 const routes = [
   {
     path: '/login',
     name: 'Login',
     component: LoginView,
-    // Não tem requiresLayout, então vai renderizar sozinho na tela
   },
   {
     path: '/register',
     name: 'Register',
     component: RegisterView,
-    // Não tem requiresLayout
   },
   {
     path: '/',
     name: 'Home',
     component: HomeView,
     meta: {
-      requiresLayout: true, // Vai renderizar DENTRO do AppLayout.vue
+      requiresLayout: true,
+      requiresAuth: true,
     },
   },
   {
@@ -33,26 +33,46 @@ const routes = [
     name: 'Transactions',
     component: TransactionsView,
     meta: {
-      requiresLayout: true, // Vai renderizar DENTRO do AppLayout.vue
+      requiresLayout: true,
+      requiresAuth: true,
     },
   },
   {
     path: '/transactions/new',
     name: 'TransactionCreate',
     component: TransactionUpsertView,
-    meta: { requiresLayout: true },
+    meta: {
+      requiresLayout: true,
+      requiresAuth: true,
+    },
   },
   {
     path: '/transactions/:id/edit',
     name: 'TransactionEdit',
     component: TransactionUpsertView,
-    meta: { requiresLayout: true },
+    meta: {
+      requiresLayout: true,
+      requiresAuth: true,
+    },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, _, next) => {
+  const authStore = useAuthStore()
+  const isAuthenticated = !!authStore.token
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'Login' })
+  } else if ((to.name === 'Login' || to.name === 'Register') && isAuthenticated) {
+    next({ name: 'Home' })
+  } else {
+    next()
+  }
 })
 
 export default router
