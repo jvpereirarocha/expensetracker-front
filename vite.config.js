5c2c72e447652fe15f45c4e756ecfb326929d5a5
Joao Victor <jvpereirarocha@gmail.com>
2026-03-12 07:41:35 -0300

    chore: add proxy

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,18 @@ export default defineConfig({
   plugins: [vue(), vueDevTools()],
   server: {
     port: 3000,
+    '/users': {
+      target: 'http://localhost:8008',
+      changeOrigin: true,
+    },
+    '/transactions': {
+      target: 'http://localhost:8008',
+      changeOrigin: true,
+    },
+    '/dashboard': {
+      target: 'http://localhost:8008',
+      changeOrigin: true,
+    },
   },
   resolve: {
     alias: {
