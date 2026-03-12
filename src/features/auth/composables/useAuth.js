0ecb08ec927be7d2d574f58d08aabf79103faf21
Joao Victor <jvpereirarocha@gmail.com>
2026-03-12 07:46:41 -0300

    feat: implement logic to handle with api calls

--- src/features/auth/composables/useAuth.js
+++ src/features/auth/composables/useAuth.js
@@ -1,31 +1,67 @@
 import { useAuthStore } from '../store/authStore'
-import { computed } from 'vue'
-// import { useRouter } from 'vue-router'
+import { computed, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export function useAuth() {
   const store = useAuthStore()
-  // const router = useRouter()
+  const router = useRouter()
 
   const isLoading = computed(() => store.isLoading)
+  const user = computed(() => store.user)
+
+  const errorMessage = ref('')
 
   const handleLogin = async (payload) => {
-    await store.login(payload)
-    // router.push({ name: 'Home' })
+    errorMessage.value = ''
+    try {
+      await store.login(payload)
+      router.push({ name: 'Home' })
+    } catch (error) {
+      handleApiError(error)
+    }
   }
 
   const handleRegister = async (payload) => {
-    if (payload.password !== payload.password_confirmation) {
-      alert('As senhas não coincidem. Por favor, verifique.')
+    errorMessage.value = ''
+    if (payload.password !== payload.confirmPassword) {
+      errorMessage.value = 'As senhas não coincidem. Por favor, verifique.'
       return
     }
+    try {
+      await store.register(payload)
+      router.push({ name: 'Home' })
+    } catch (error) {
+      handleApiError(error)
+    }
+  }
 
-    await store.register(payload)
-    // router.push({ name: 'Home' })
+  const handleApiError = (error) => {
+    if (error.response && error.response.data) {
+      const detail = error.response.data.detail
+
+      if (Array.isArray(detail)) {
+        errorMessage.value = detail[0].msg
+      } else if (typeof detail === 'string') {
+        errorMessage.value = detail
+      } else {
+        errorMessage.value = 'Erro de validação nos dados enviados.'
+      }
+    } else {
+      errorMessage.value = 'Ocorreu um erro inesperado. Tente novamente.'
+    }
+  }
+
+  const handleLogout = () => {
+    store.logout()
+    router.push({ name: 'Login' })
   }
 
   return {
+    user,
     isLoading,
     handleLogin,
     handleRegister,
+    handleLogout,
+    errorMessage,
   }
 }
