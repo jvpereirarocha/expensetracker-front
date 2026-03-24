678db751cece00eeb5249e84955622891b7784c8
Joao Victor <jvpereirarocha@gmail.com>
2026-03-24 19:00:11 -0300

    refactor: preparing project to deploy

--- vite.config.js
+++ vite.config.js
@@ -9,17 +9,19 @@ export default defineConfig({
   plugins: [vue(), vueDevTools()],
   server: {
     port: 3000,
-    '/users': {
-      target: 'http://localhost:8008',
-      changeOrigin: true,
-    },
-    '/transactions': {
-      target: 'http://localhost:8008',
-      changeOrigin: true,
-    },
-    '/dashboard': {
-      target: 'http://localhost:8008',
-      changeOrigin: true,
+    proxy: {
+      '/users': {
+        target: 'http://localhost:8008',
+        changeOrigin: true,
+      },
+      '/transactions': {
+        target: 'http://localhost:8008',
+        changeOrigin: true,
+      },
+      '/dashboard': {
+        target: 'http://localhost:8008',
+        changeOrigin: true,
+      },
     },
   },
   resolve: {
